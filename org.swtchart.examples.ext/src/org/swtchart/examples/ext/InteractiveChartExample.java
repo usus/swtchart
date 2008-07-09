025c5e47a0253f6d7c460f0f05ab4cf30d15695a
yoshitaka <yoshitaka@d30752db-1043-0410-9340-8085d4ee3fb9>
2008-07-09 23:25:24 +0000

    git-svn-id: https://swt-chart.svn.sourceforge.net/svnroot/swt-chart/trunk@34 d30752db-1043-0410-9340-8085d4ee3fb9

--- org.swtchart.examples.ext/src/org/swtchart/examples/ext/InteractiveChartExample.java
+++ org.swtchart.examples.ext/src/org/swtchart/examples/ext/InteractiveChartExample.java
@@ -1,11 +1,12 @@
 package org.swtchart.examples.ext;
 
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.part.ViewPart;
 import org.swtchart.Chart;
-import org.swtchart.Constants;
 import org.swtchart.IBarSeries;
 import org.swtchart.ILineSeries;
 import org.swtchart.ISeries.SeriesType;
@@ -16,8 +17,9 @@ import org.swtchart.ext.InteractiveChart;
  */
 public class InteractiveChartExample extends ViewPart {
 
-	private static final String[] categorySeries = { "Mon", "Tue", "Wed", "Thu", "Fri" };
-	private static final double[] yLineSeries1 = { 4.6, 5.4, 6.9,5.6, 7.1 };
+	private static final String[] categorySeries = { "Mon", "Tue", "Wed",
+			"Thu", "Fri" };
+	private static final double[] yLineSeries1 = { 4.6, 5.4, 6.9, 5.6, 7.1 };
 	private static final double[] yLineSeries2 = { 6.0, 5.1, 4.9, 5.3, 4.2 };
 	private static final double[] yBarSeries1 = { 1.1, 2.9, 3.3, 4.4, 3.5 };
 	private static final double[] yBarSeries2 = { 4.3, 3.4, 2.8, 2.1, 1.9 };
@@ -44,27 +46,27 @@ public class InteractiveChartExample extends ViewPart {
 		chart.getAxisSet().getXAxis(0).setCategorySeries(categorySeries);
 
 		// create line series 1
-		ILineSeries lineSeries1 = (ILineSeries) chart.getSeriesSet().createSeries(
-				SeriesType.LINE, "line series 1");
+		ILineSeries lineSeries1 = (ILineSeries) chart.getSeriesSet()
+				.createSeries(SeriesType.LINE, "line series 1");
 		lineSeries1.setYSeries(yLineSeries1);
-		
+
 		// create line series 2
-		ILineSeries lineSeries2 = (ILineSeries) chart.getSeriesSet().createSeries(
-				SeriesType.LINE, "line series 2");
+		ILineSeries lineSeries2 = (ILineSeries) chart.getSeriesSet()
+				.createSeries(SeriesType.LINE, "line series 2");
 		lineSeries2.setYSeries(yLineSeries2);
-		lineSeries2.setLineColor(Constants.RED);
+		lineSeries2.setLineColor(new Color(Display.getDefault(), 255, 0, 0));
 
 		// create bar series 1
-		IBarSeries barSeries1 = (IBarSeries) chart.getSeriesSet().createSeries(SeriesType.BAR,
-				"bar series 1");
+		IBarSeries barSeries1 = (IBarSeries) chart.getSeriesSet().createSeries(
+				SeriesType.BAR, "bar series 1");
 		barSeries1.setYSeries(yBarSeries1);
-		
+
 		// create bar series 2
-		IBarSeries barSeries2 = (IBarSeries) chart.getSeriesSet().createSeries(SeriesType.BAR,
-				"bar series 2");
+		IBarSeries barSeries2 = (IBarSeries) chart.getSeriesSet().createSeries(
+				SeriesType.BAR, "bar series 2");
 		barSeries2.setYSeries(yBarSeries2);
-		barSeries2.setBarColor(Constants.LIGHT_GREEN);
-		
+		barSeries2.setBarColor(new Color(Display.getDefault(), 80, 240, 180));
+
 		// fit the axis scale
 		chart.getAxisSet().autoScale();
 	}
