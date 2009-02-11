686afe3316a1d8a8d6e47f4c71429befb1abc73b
yoshitaka <yoshitaka@d30752db-1043-0410-9340-8085d4ee3fb9>
2009-02-11 23:41:35 +0000

    git-svn-id: https://swt-chart.svn.sourceforge.net/svnroot/swt-chart/trunk@111 d30752db-1043-0410-9340-8085d4ee3fb9

--- org.swtchart/src/org/swtchart/internal/Title.java
+++ org.swtchart/src/org/swtchart/internal/Title.java
@@ -21,223 +21,207 @@ import org.swtchart.ITitle;
  */
 public class Title extends Canvas implements ITitle, PaintListener {
 
-	/** the chart */
-	protected Chart chart;
+    /** the chart */
+    protected Chart chart;
 
-	/** the title text */
-	protected String text;
+    /** the title text */
+    protected String text;
 
-	/** the visibility state of axis */
-	protected boolean isVisible;
+    /** the visibility state of axis */
+    protected boolean isVisible;
 
-	/** the default font */
-	private static final int DEFAULT_FONT_SIZE = Constants.LARGE_FONT_SIZE;
+    /** the default font */
+    private static final int DEFAULT_FONT_SIZE = Constants.LARGE_FONT_SIZE;
 
-	/** the default color */
-	private static final RGB DEFAULT_FOREGROUND = Constants.BLUE;
+    /** the default color */
+    private static final RGB DEFAULT_FOREGROUND = Constants.BLUE;
 
-	/** the default text */
-	private static final String DEFAULT_TEXT = "";
+    /** the default text */
+    private static final String DEFAULT_TEXT = "";
 
-	/**
-	 * Constructor.
-	 * 
-	 * @param parent
-	 *            the parent composite
-	 * @param style
-	 *            the style
-	 * @param chart
-	 *            the chart
-	 */
-	public Title(Chart parent, int style) {
-		super(parent, style);
+    /**
+     * Constructor.
+     * 
+     * @param parent
+     *            the parent composite
+     * @param style
+     *            the style
+     * @param chart
+     *            the chart
+     */
+    public Title(Chart parent, int style) {
+        super(parent, style);
 
-		this.chart = parent;
-		text = getDefaultText();
-		isVisible = true;
+        this.chart = parent;
+        text = getDefaultText();
+        isVisible = true;
 
-		setFont(new Font(Display.getDefault(), "Tahoma", DEFAULT_FONT_SIZE,
-				SWT.BOLD));
-		setForeground(new Color(Display.getDefault(), DEFAULT_FOREGROUND));
+        setFont(new Font(Display.getDefault(), "Tahoma", DEFAULT_FONT_SIZE,
+                SWT.BOLD));
+        setForeground(new Color(Display.getDefault(), DEFAULT_FOREGROUND));
 
-		addPaintListener(this);
-	}
+        addPaintListener(this);
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.swtchart.ITitle#setText(java.lang.String)
-	 */
-	public void setText(String text) {
-		if (text == null) {
-			this.text = getDefaultText();
-		} else {
-			this.text = text;
-		}
-		chart.updateLayout(); // text could be changed to blank
-	}
+    /*
+     * @see ITitle#setText(String)
+     */
+    public void setText(String text) {
+        if (text == null) {
+            this.text = getDefaultText();
+        } else {
+            this.text = text;
+        }
+        chart.updateLayout(); // text could be changed to blank
+    }
 
-	/**
-	 * Gets the default title text.
-	 * 
-	 * @return the default title text
-	 */
-	protected String getDefaultText() {
-		return DEFAULT_TEXT;
-	}
+    /**
+     * Gets the default title text.
+     * 
+     * @return the default title text
+     */
+    protected String getDefaultText() {
+        return DEFAULT_TEXT;
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.swtchart.ITitle#getText()
-	 */
-	public String getText() {
-		return text;
-	}
+    /*
+     * @see ITitle#getText()
+     */
+    public String getText() {
+        return text;
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * org.eclipse.swt.widgets.Canvas#setFont(org.eclipse.swt.graphics.Font)
-	 */
-	@Override
-	public void setFont(Font font) {
-		if (font == null) {
-			font = new Font(Display.getDefault(), "Tahoma", DEFAULT_FONT_SIZE,
-					SWT.BOLD);
-		}
-		super.setFont(font);
-		chart.updateLayout();
-	}
+    /*
+     * @see Canvas#setFont(Font)
+     */
+    @Override
+    public void setFont(Font font) {
+        if (font == null) {
+            super.setFont(new Font(Display.getDefault(), "Tahoma",
+                    DEFAULT_FONT_SIZE, SWT.BOLD));
+        } else {
+            super.setFont(font);
+        }
+        chart.updateLayout();
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * org.eclipse.swt.widgets.Control#setForeground(org.eclipse.swt.graphics
-	 * .Color)
-	 */
-	@Override
-	public void setForeground(Color color) {
-		if (color == null) {
-			color = new Color(Display.getDefault(), DEFAULT_FOREGROUND);
-		}
-		super.setForeground(color);
-	}
+    /*
+     * @see Control#setForeground(Color)
+     */
+    @Override
+    public void setForeground(Color color) {
+        if (color == null) {
+            super.setForeground(new Color(Display.getDefault(),
+                    DEFAULT_FOREGROUND));
+        } else {
+            super.setForeground(color);
+        }
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.eclipse.swt.widgets.Control#setVisible(boolean)
-	 */
-	@Override
-	public void setVisible(boolean isVisible) {
-		if (this.isVisible == isVisible) {
-			return;
-		}
+    /*
+     * @see Control#setVisible(boolean)
+     */
+    @Override
+    public void setVisible(boolean isVisible) {
+        if (this.isVisible == isVisible) {
+            return;
+        }
 
-		this.isVisible = isVisible;
-		chart.updateLayout();
-	}
+        this.isVisible = isVisible;
+        chart.updateLayout();
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.eclipse.swt.widgets.Control#isVisible()
-	 */
-	@Override
-	public boolean isVisible() {
-		return isVisible;
-	}
+    /*
+     * @see Control#isVisible()
+     */
+    @Override
+    public boolean isVisible() {
+        return isVisible;
+    }
 
-	/**
-	 * Gets the state indicating if showing title horizontally.
-	 * 
-	 * @return the state indicating if showing title horizontally
-	 */
-	protected boolean isHorizontal() {
-		return true;
-	}
+    /**
+     * Gets the state indicating if showing title horizontally.
+     * 
+     * @return the state indicating if showing title horizontally
+     */
+    protected boolean isHorizontal() {
+        return true;
+    }
 
-	/**
-	 * Updates the title layout data.
-	 */
-	public void updateLayoutData() {
-		int height;
-		int width;
-		if (isVisible() && !text.trim().equals("")) {
-			Point p = Util.getExtentInGC(getFont(), text);
-			width = p.x;
-			height = p.y;
-		} else {
-			width = 0;
-			height = 0;
-		}
-		if (isHorizontal()) {
-			setLayoutData(new ChartLayoutData(width, height));
-		} else {
-			setLayoutData(new ChartLayoutData(height, width));
-		}
-	}
+    /**
+     * Updates the title layout data.
+     */
+    public void updateLayoutData() {
+        int height;
+        int width;
+        if (isVisible() && !text.trim().equals("")) {
+            Point p = Util.getExtentInGC(getFont(), text);
+            width = p.x;
+            height = p.y;
+        } else {
+            width = 0;
+            height = 0;
+        }
+        if (isHorizontal()) {
+            setLayoutData(new ChartLayoutData(width, height));
+        } else {
+            setLayoutData(new ChartLayoutData(height, width));
+        }
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * org.eclipse.swt.events.PaintListener#paintControl(org.eclipse.swt.events
-	 * .PaintEvent)
-	 */
-	public void paintControl(PaintEvent e) {
+    /*
+     * @see PaintListener#paintControl(PaintEvent)
+     */
+    public void paintControl(PaintEvent e) {
 
-		if (text == null || text.equals("") || !isVisible) {
-			return;
-		}
+        if (text == null || text.equals("") || !isVisible) {
+            return;
+        }
 
-		int width = getSize().x;
-		int height = getSize().y;
-		GC gc = e.gc;
-		gc.setForeground(getForeground());
-		gc.setFont(getFont());
+        int width = getSize().x;
+        int height = getSize().y;
+        GC gc = e.gc;
+        gc.setForeground(getForeground());
+        gc.setFont(getFont());
 
-		if (isHorizontal()) {
-			int textWidth = gc.textExtent(text).x;
-			int x = (int) (width / 2.0 - textWidth / 2.0);
-			if (x < 0) {
-				// this happens when window size is too small
-				x = 0;
-			}
+        if (isHorizontal()) {
+            int textWidth = gc.textExtent(text).x;
+            int x = (int) (width / 2.0 - textWidth / 2.0);
+            if (x < 0) {
+                // this happens when window size is too small
+                x = 0;
+            }
 
-			gc.drawString(text, x, 0, true);
-		} else {
-			int textWidth = 0;
-			int textHeight = 0;
+            gc.drawString(text, x, 0, true);
+        } else {
+            int textWidth = 0;
+            int textHeight = 0;
 
-			gc.setFont(getFont());
-			Color background = gc.getBackground();
-			textWidth = gc.textExtent(text).x;
-			textHeight = gc.textExtent(text).y;
-			Image image = new Image(Display.getCurrent(), textWidth, textHeight);
-			GC tmpGc = new GC(image);
-			tmpGc.setForeground(getForeground());
-			tmpGc.setBackground(background);
-			tmpGc.setFont(getFont());
-			tmpGc.drawText(text, 0, 0);
-			ImageData imageData = Util.rotate(image.getImageData());
-			image.dispose();
-			tmpGc.dispose();
+            gc.setFont(getFont());
+            Color background = gc.getBackground();
+            textWidth = gc.textExtent(text).x;
+            textHeight = gc.textExtent(text).y;
+            Image image = new Image(Display.getCurrent(), textWidth, textHeight);
+            GC tmpGc = new GC(image);
+            tmpGc.setForeground(getForeground());
+            tmpGc.setBackground(background);
+            tmpGc.setFont(getFont());
+            tmpGc.drawText(text, 0, 0);
+            ImageData imageData = Util.rotate(image.getImageData());
+            image.dispose();
+            tmpGc.dispose();
 
-			if (imageData == null) {
-				return;
-			}
+            if (imageData == null) {
+                return;
+            }
 
-			int y = (int) (height / 2.0 - textWidth / 2.0);
-			if (y < 0) {
-				y = 0;
-			}
-			image = new Image(Display.getCurrent(), imageData);
-			e.gc.drawImage(image, 0, y);
-			image.dispose();
-		}
-	}
+            int y = (int) (height / 2.0 - textWidth / 2.0);
+            if (y < 0) {
+                y = 0;
+            }
+            image = new Image(Display.getCurrent(), imageData);
+            e.gc.drawImage(image, 0, y);
+            image.dispose();
+        }
+    }
 }
