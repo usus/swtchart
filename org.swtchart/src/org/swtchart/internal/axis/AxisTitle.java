8de8c3e72c3e7eed41e70c8a76591d62d646fd67
yoshitaka <yoshitaka@d30752db-1043-0410-9340-8085d4ee3fb9>
2009-02-11 23:56:06 +0000

    git-svn-id: https://swt-chart.svn.sourceforge.net/svnroot/swt-chart/trunk@121 d30752db-1043-0410-9340-8085d4ee3fb9

--- org.swtchart/src/org/swtchart/internal/axis/AxisTitle.java
+++ org.swtchart/src/org/swtchart/internal/axis/AxisTitle.java
@@ -13,58 +13,54 @@ import org.swtchart.internal.Title;
  */
 public class AxisTitle extends Title {
 
-	/** the axis */
-	private Axis axis;
+    /** the axis */
+    private Axis axis;
 
-	/** the default text for X Axis */
-	private static final String DEFAULT_TEXT_FOR_XAXIS = "X Axis";
+    /** the default text for X Axis */
+    private static final String DEFAULT_TEXT_FOR_XAXIS = "X Axis";
 
-	/** the default text for X Axis */
-	private static final String DEFAULT_TEXT_FOR_YAXIS = "Y Axis";
+    /** the default text for X Axis */
+    private static final String DEFAULT_TEXT_FOR_YAXIS = "Y Axis";
 
-	/** the direction of axis */
-	private Direction direction;
+    /** the direction of axis */
+    private Direction direction;
 
-	/**
-	 * Constructor.
-	 * 
-	 * @param chart
-	 *            the chart
-	 * @param style
-	 *            the style
-	 * @param axis
-	 *            the axis
-	 * @param direction
-	 *            the direction
-	 */
-	public AxisTitle(Chart chart, int style, Axis axis, Direction direction) {
-		super(chart, style);
-		this.axis = axis;
-		this.direction = direction;
-		setFont(new Font(Display.getDefault(), "Tahoma",
-				Constants.MEDIUM_FONT_SIZE, SWT.BOLD));
-	}
+    /**
+     * Constructor.
+     * 
+     * @param chart
+     *            the chart
+     * @param style
+     *            the style
+     * @param axis
+     *            the axis
+     * @param direction
+     *            the direction
+     */
+    public AxisTitle(Chart chart, int style, Axis axis, Direction direction) {
+        super(chart, style);
+        this.axis = axis;
+        this.direction = direction;
+        setFont(new Font(Display.getDefault(), "Tahoma",
+                Constants.MEDIUM_FONT_SIZE, SWT.BOLD));
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.swtchart.internal.Title#getDefaultText()
-	 */
-	@Override
-	protected String getDefaultText() {
-		if (direction == Direction.X) {
-			return DEFAULT_TEXT_FOR_XAXIS;
-		}
-		return DEFAULT_TEXT_FOR_YAXIS;
-	}
+    /*
+     * @see Title#getDefaultText()
+     */
+    @Override
+    protected String getDefaultText() {
+        if (direction == Direction.X) {
+            return DEFAULT_TEXT_FOR_XAXIS;
+        }
+        return DEFAULT_TEXT_FOR_YAXIS;
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.swtchart.internal.Title#isHorizontal()
-	 */
-	@Override
-	protected boolean isHorizontal() {
-		return axis.isHorizontalAxis();
-	}
+    /*
+     * @see Title#isHorizontal()
+     */
+    @Override
+    protected boolean isHorizontal() {
+        return axis.isHorizontalAxis();
+    }
 }
