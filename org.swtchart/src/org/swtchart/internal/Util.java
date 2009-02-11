cb1d6308601df88ad17ab4d4c58355191a43d0a3
yoshitaka <yoshitaka@d30752db-1043-0410-9340-8085d4ee3fb9>
2009-02-11 23:43:53 +0000

    git-svn-id: https://swt-chart.svn.sourceforge.net/svnroot/swt-chart/trunk@115 d30752db-1043-0410-9340-8085d4ee3fb9

--- org.swtchart/src/org/swtchart/internal/Util.java
+++ org.swtchart/src/org/swtchart/internal/Util.java
@@ -14,101 +14,101 @@ import org.swtchart.LineStyle;
  */
 public final class Util {
 
-	/**
-	 * Rotates the given image data. If the given image data is
-	 * <code>null</code>, <code>null</code> will be returned.
-	 * 
-	 * @param imageData
-	 *            the image data to be rotated
-	 * @return the rotated image data
-	 * @throws IllegalArgumentException
-	 *             if the given image data is <code>null</code>.
-	 */
-	public static ImageData rotate(ImageData imageData)
-			throws IllegalArgumentException {
+    /**
+     * Rotates the given image data. If the given image data is
+     * <code>null</code>, <code>null</code> will be returned.
+     * 
+     * @param imageData
+     *            the image data to be rotated
+     * @return the rotated image data
+     * @throws IllegalArgumentException
+     *             if the given image data is <code>null</code>.
+     */
+    public static ImageData rotate(ImageData imageData)
+            throws IllegalArgumentException {
 
-		if (imageData == null) {
-			return null;
-		}
+        if (imageData == null) {
+            return null;
+        }
 
-		int scanlinePad = imageData.height * imageData.bytesPerLine
-				/ imageData.width;
-		byte[] data = new byte[imageData.data.length];
+        int scanlinePad = imageData.height * imageData.bytesPerLine
+                / imageData.width;
+        byte[] data = new byte[imageData.data.length];
 
-		for (int y = 0; y < imageData.height; y++) {
-			for (int x = 0; x < imageData.width; x++) {
-				int srcPos = y * imageData.bytesPerLine + x
-						* imageData.bytesPerLine / imageData.width;
-				int destPos = (imageData.width - x - 1) * scanlinePad + y
-						* imageData.bytesPerLine / imageData.width;
-				int length = imageData.bytesPerLine / imageData.width;
+        for (int y = 0; y < imageData.height; y++) {
+            for (int x = 0; x < imageData.width; x++) {
+                int srcPos = y * imageData.bytesPerLine + x
+                        * imageData.bytesPerLine / imageData.width;
+                int destPos = (imageData.width - x - 1) * scanlinePad + y
+                        * imageData.bytesPerLine / imageData.width;
+                int length = imageData.bytesPerLine / imageData.width;
 
-				System.arraycopy(imageData.data, srcPos, data, destPos, length);
-			}
-		}
+                System.arraycopy(imageData.data, srcPos, data, destPos, length);
+            }
+        }
 
-		return new ImageData(imageData.height, imageData.width,
-				imageData.depth, imageData.palette, scanlinePad, data);
-	}
+        return new ImageData(imageData.height, imageData.width,
+                imageData.depth, imageData.palette, scanlinePad, data);
+    }
 
-	/**
-	 * Gets the text extent with given font in GC. If the given text or font is
-	 * <code>null</code> or already disposed, point containing size zero will
-	 * be returned.
-	 * 
-	 * @param font
-	 *            the font
-	 * @param text
-	 *            the text
-	 * @return a point containing text extent
-	 */
-	public static Point getExtentInGC(Font font, String text) {
+    /**
+     * Gets the text extent with given font in GC. If the given text or font is
+     * <code>null</code> or already disposed, point containing size zero will be
+     * returned.
+     * 
+     * @param font
+     *            the font
+     * @param text
+     *            the text
+     * @return a point containing text extent
+     */
+    public static Point getExtentInGC(Font font, String text) {
 
-		if (text == null || font == null || font.isDisposed()) {
-			return new Point(0, 0);
-		}
+        if (text == null || font == null || font.isDisposed()) {
+            return new Point(0, 0);
+        }
 
-		// create GC
-		int ARBITRARY_WIDTH = 10;
-		int ARBITRARY_HEIGHT = 10;
-		Image image = new Image(Display.getCurrent(), ARBITRARY_WIDTH,
-				ARBITRARY_HEIGHT);
-		GC gc = new GC(image);
+        // create GC
+        int ARBITRARY_WIDTH = 10;
+        int ARBITRARY_HEIGHT = 10;
+        Image image = new Image(Display.getCurrent(), ARBITRARY_WIDTH,
+                ARBITRARY_HEIGHT);
+        GC gc = new GC(image);
 
-		// get extent of text with given font
-		gc.setFont(font);
-		Point p = gc.textExtent(text);
+        // get extent of text with given font
+        gc.setFont(font);
+        Point p = gc.textExtent(text);
 
-		// dispose resources
-		image.dispose();
-		gc.dispose();
+        // dispose resources
+        image.dispose();
+        gc.dispose();
 
-		return p;
-	}
-	
-	/**
-	 * Gets the index defined in SWT.
-	 * 
-	 * @param lineStyle
-	 *            the line style
-	 * @return the index defined in SWT.
-	 */
-	public static int getIndexDefinedInSWT(LineStyle lineStyle) {
-		switch (lineStyle) {
-		case NONE:
-			return SWT.NONE;
-		case SOLID:
-			return SWT.LINE_SOLID;
-		case DASH:
-			return SWT.LINE_DASH;
-		case DOT:
-			return SWT.LINE_DOT;
-		case DASHDOT:
-			return SWT.LINE_DASHDOT;
-		case DASHDOTDOT:
-			return SWT.LINE_DASHDOTDOT;
-		default:
-			return SWT.LINE_SOLID;
-		}
-	}
+        return p;
+    }
+
+    /**
+     * Gets the index defined in SWT.
+     * 
+     * @param lineStyle
+     *            the line style
+     * @return the index defined in SWT.
+     */
+    public static int getIndexDefinedInSWT(LineStyle lineStyle) {
+        switch (lineStyle) {
+        case NONE:
+            return SWT.NONE;
+        case SOLID:
+            return SWT.LINE_SOLID;
+        case DASH:
+            return SWT.LINE_DASH;
+        case DOT:
+            return SWT.LINE_DOT;
+        case DASHDOT:
+            return SWT.LINE_DASHDOT;
+        case DASHDOTDOT:
+            return SWT.LINE_DASHDOTDOT;
+        default:
+            return SWT.LINE_SOLID;
+        }
+    }
 }
