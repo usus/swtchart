1e81f62a92ba98db16a015f2f84395c45af491e4
yoshitaka <yoshitaka@d30752db-1043-0410-9340-8085d4ee3fb9>
2009-02-11 23:34:52 +0000

    git-svn-id: https://swt-chart.svn.sourceforge.net/svnroot/swt-chart/trunk@108 d30752db-1043-0410-9340-8085d4ee3fb9

--- org.swtchart/src/org/swtchart/internal/Grid.java
+++ org.swtchart/src/org/swtchart/internal/Grid.java
@@ -17,150 +17,142 @@ import org.swtchart.internal.axis.Axis;
  */
 public class Grid implements IGrid {
 
-	/** the axis */
-	private Axis axis;
+    /** the axis */
+    private Axis axis;
 
-	/** the grid color */
-	private Color color;
+    /** the grid color */
+    private Color color;
 
-	/** the visibility state */
-	private boolean isVisible;
+    /** the visibility state */
+    private boolean isVisible;
 
-	/** the line style */
-	private LineStyle lineStyle;
+    /** the line style */
+    private LineStyle lineStyle;
 
-	/** the line width */
-	private final static int LINE_WIDTH = 1;
+    /** the line width */
+    private final static int LINE_WIDTH = 1;
 
-	/** the default style */
-	private final static LineStyle DEFAULT_STYLE = LineStyle.DOT;
+    /** the default style */
+    private final static LineStyle DEFAULT_STYLE = LineStyle.DOT;
 
-	/** the default color */
-	private final static RGB DEFAULT_FOREGROUND = Constants.GRAY;
+    /** the default color */
+    private final static RGB DEFAULT_FOREGROUND = Constants.GRAY;
 
-	/**
-	 * Constructor.
-	 * 
-	 * @param axis
-	 *            the axis
-	 */
-	public Grid(Axis axis) {
-		this.axis = axis;
+    /**
+     * Constructor.
+     * 
+     * @param axis
+     *            the axis
+     */
+    public Grid(Axis axis) {
+        this.axis = axis;
 
-		color = new Color(Display.getDefault(), DEFAULT_FOREGROUND);
-		lineStyle = DEFAULT_STYLE;
-		isVisible = true;
-	}
+        color = new Color(Display.getDefault(), DEFAULT_FOREGROUND);
+        lineStyle = DEFAULT_STYLE;
+        isVisible = true;
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.swtchart.IGrid#getForeground()
-	 */
-	public Color getForeground() {
-		return color;
-	}
+    /*
+     * @see IGrid#getForeground()
+     */
+    public Color getForeground() {
+        return color;
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.swtchart.IGrid#setForeground(org.eclipse.swt.graphics.Color)
-	 */
-	public void setForeground(Color color) {
-		if (color != null && color.isDisposed()) {
-			SWT.error(SWT.ERROR_INVALID_ARGUMENT);
-		}
+    /*
+     * @see IGrid#setForeground(Color)
+     */
+    public void setForeground(Color color) {
+        if (color != null && color.isDisposed()) {
+            SWT.error(SWT.ERROR_INVALID_ARGUMENT);
+        }
 
-		if (color == null) {
-			color = new Color(Display.getDefault(), DEFAULT_FOREGROUND);
-		}
+        if (color == null) {
+            this.color = new Color(Display.getDefault(), DEFAULT_FOREGROUND);
+        } else {
+            this.color = color;
+        }
+    }
 
-		this.color = color;
-	}
+    /*
+     * @see IGrid#getStyle()
+     */
+    public LineStyle getStyle() {
+        return lineStyle;
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.swtchart.IGrid#getStyle()
-	 */
-	public LineStyle getStyle() {
-		return lineStyle;
-	}
+    /*
+     * @see IGrid#setStyle(LineStyle)
+     */
+    public void setStyle(LineStyle style) {
+        if (style == null) {
+            this.lineStyle = DEFAULT_STYLE;
+        } else {
+            this.lineStyle = style;
+        }
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.swtchart.IGrid#setStyle(org.swtchart.LineStyle)
-	 */
-	public void setStyle(LineStyle style) {
-		if (style == null) {
-			style = DEFAULT_STYLE;
-		}
+    /**
+     * Draws grid.
+     * 
+     * @param gc
+     *            the graphics context
+     * @param width
+     *            the width to draw grid
+     * @param height
+     *            the height to draw grid
+     */
+    protected void draw(GC gc, int width, int height) {
+        if (!isVisible || lineStyle.equals(LineStyle.NONE)) {
+            return;
+        }
 
-		this.lineStyle = style;
-	}
+        int xWidth;
+        if (axis.isHorizontalAxis()) {
+            xWidth = width;
+        } else {
+            xWidth = height;
+        }
 
-	/**
-	 * Draws grid.
-	 * 
-	 * @param gc
-	 *            the graphics context
-	 * @param width
-	 *            the width to draw grid
-	 * @param height
-	 *            the height to draw grid
-	 */
-	protected void draw(GC gc, int width, int height) {
-		if (!isVisible || lineStyle.equals(LineStyle.NONE)) {
-			return;
-		}
+        gc.setForeground(color);
+        ArrayList<Integer> tickLabelPosition = axis.getTick()
+                .getAxisTickLabels().getTickLabelPositions();
 
-		int xWidth;
-		if (axis.isHorizontalAxis()) {
-			xWidth = width;
-		} else {
-			xWidth = height;
-		}
+        gc.setLineStyle(Util.getIndexDefinedInSWT(lineStyle));
+        if (axis.isValidCategoryAxis()) {
+            int step = 0;
+            if (tickLabelPosition.size() > 1) {
+                step = tickLabelPosition.get(1).intValue()
+                        - tickLabelPosition.get(0).intValue();
+            }
+            int x = (int) (tickLabelPosition.get(0).intValue() - step / 2d);
 
-		gc.setForeground(color);
-		ArrayList<Integer> tickLabelPosition = axis.getTick()
-				.getAxisTickLabels().getTickLabelPositions();
+            for (int i = 0; i < tickLabelPosition.size() + 1; i++) {
+                x += step;
+                if (x >= xWidth) {
+                    continue;
+                }
 
-		gc.setLineStyle(Util.getIndexDefinedInSWT(lineStyle));
-		if (axis.isValidCategoryAxis()) {
-			int step = 0;
-			if (tickLabelPosition.size() > 1) {
-				step = tickLabelPosition.get(1).intValue()
-						- tickLabelPosition.get(0).intValue();
-			}
-			int x = (int) (tickLabelPosition.get(0).intValue() - step / 2d);
+                if (axis.isHorizontalAxis()) {
+                    gc.drawLine(x, LINE_WIDTH, x, height - LINE_WIDTH);
+                } else {
+                    gc.drawLine(LINE_WIDTH, x, width - LINE_WIDTH, x);
+                }
+            }
+        } else {
+            for (int i = 0; i < tickLabelPosition.size(); i++) {
+                int x = tickLabelPosition.get(i).intValue();
+                if (x >= xWidth) {
+                    continue;
+                }
 
-			for (int i = 0; i < tickLabelPosition.size() + 1; i++) {
-				x += step;
-				if (x >= xWidth) {
-					continue;
-				}
-
-				if (axis.isHorizontalAxis()) {
-					gc.drawLine(x, LINE_WIDTH, x, height - LINE_WIDTH);
-				} else {
-					gc.drawLine(LINE_WIDTH, x, width - LINE_WIDTH, x);
-				}
-			}
-		} else {
-			for (int i = 0; i < tickLabelPosition.size(); i++) {
-				int x = tickLabelPosition.get(i).intValue();
-				if (x >= xWidth) {
-					continue;
-				}
-
-				if (axis.isHorizontalAxis()) {
-					gc.drawLine(x, LINE_WIDTH, x, height - LINE_WIDTH);
-				} else {
-					gc.drawLine(LINE_WIDTH, height - 1 - x, width - LINE_WIDTH,
-							height - 1 - x);
-				}
-			}
-		}
-	}
+                if (axis.isHorizontalAxis()) {
+                    gc.drawLine(x, LINE_WIDTH, x, height - LINE_WIDTH);
+                } else {
+                    gc.drawLine(LINE_WIDTH, height - 1 - x, width - LINE_WIDTH,
+                            height - 1 - x);
+                }
+            }
+        }
+    }
 }
