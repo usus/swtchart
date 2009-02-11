7418dd0b10a2c8c167bbe06a508923a3de480344
yoshitaka <yoshitaka@d30752db-1043-0410-9340-8085d4ee3fb9>
2009-02-11 23:10:07 +0000

    git-svn-id: https://swt-chart.svn.sourceforge.net/svnroot/swt-chart/trunk@105 d30752db-1043-0410-9340-8085d4ee3fb9

--- org.swtchart/src/org/swtchart/internal/series/SeriesLabel.java
+++ org.swtchart/src/org/swtchart/internal/series/SeriesLabel.java
@@ -16,150 +16,133 @@ import org.swtchart.ISeriesLabel;
  */
 public class SeriesLabel implements ISeriesLabel {
 
-	/** the visibility state of series label */
-	private boolean isVisible;
+    /** the visibility state of series label */
+    private boolean isVisible;
 
-	/** the series label font */
-	protected Font font;
+    /** the series label font */
+    protected Font font;
 
-	/** the series label color */
-	protected Color color;
+    /** the series label color */
+    protected Color color;
 
-	/** the format for series label */
-	private String format;
+    /** the format for series label */
+    private String format;
 
-	/** the default label color */
-	private static final RGB DEFAULT_COLOR = Constants.BLACK;
+    /** the default label color */
+    private static final RGB DEFAULT_COLOR = Constants.BLACK;
 
-	/** the default label format */
-	private static final String DEFAULT_FORMAT = "############.###########";
+    /** the default label format */
+    private static final String DEFAULT_FORMAT = "############.###########";
 
-	/** the default font */
-	private static final int DEFAULT_FONT_SIZE = Constants.SMALL_FONT_SIZE;
+    /** the default font */
+    private static final int DEFAULT_FONT_SIZE = Constants.SMALL_FONT_SIZE;
 
-	/**
-	 * Constructor.
-	 */
-	public SeriesLabel() {
-		font = new Font(Display.getDefault(), "Tahoma", DEFAULT_FONT_SIZE,
-				SWT.NORMAL);
-		color = new Color(Display.getDefault(), DEFAULT_COLOR);
-		isVisible = false;
-		format = DEFAULT_FORMAT;
-	}
+    /**
+     * Constructor.
+     */
+    public SeriesLabel() {
+        font = new Font(Display.getDefault(), "Tahoma", DEFAULT_FONT_SIZE,
+                SWT.NORMAL);
+        color = new Color(Display.getDefault(), DEFAULT_COLOR);
+        isVisible = false;
+        format = DEFAULT_FORMAT;
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.swtchart.ISeriesLabel#getFormat()
-	 */
-	public String getFormat() {
-		return format;
-	}
+    /*
+     * @see ISeriesLabel#getFormat()
+     */
+    public String getFormat() {
+        return format;
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.swtchart.ISeriesLabel#setFormat(java.lang.String)
-	 */
-	public void setFormat(String format) {
-		if (format == null) {
-			this.format = DEFAULT_FORMAT;
-		} else {
-			this.format = format;
-		}
-	}
+    /*
+     * @see ISeriesLabel#setFormat(String)
+     */
+    public void setFormat(String format) {
+        if (format == null) {
+            this.format = DEFAULT_FORMAT;
+        } else {
+            this.format = format;
+        }
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.swtchart.ISeriesLabel#getForeground()
-	 */
-	public Color getForeground() {
-		return color;
-	}
+    /*
+     * @see ISeriesLabel#getForeground()
+     */
+    public Color getForeground() {
+        return color;
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * org.swtchart.ISeriesLabel#setForeground(org.eclipse.swt.graphics.Color)
-	 */
-	public void setForeground(Color color) {
-		if (color != null && color.isDisposed()) {
-			SWT.error(SWT.ERROR_INVALID_ARGUMENT);
-		}
+    /*
+     * @see ISeriesLabel#setForeground(Color)
+     */
+    public void setForeground(Color color) {
+        if (color != null && color.isDisposed()) {
+            SWT.error(SWT.ERROR_INVALID_ARGUMENT);
+        }
 
-		if (color == null) {
-			color = new Color(Display.getDefault(), DEFAULT_COLOR);
-		}
+        if (color == null) {
+            this.color = new Color(Display.getDefault(), DEFAULT_COLOR);
+        } else {
+            this.color = color;
+        }
+    }
 
-		this.color = color;
-	}
+    /*
+     * @see ISeriesLabel#getFont()
+     */
+    public Font getFont() {
+        return font;
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.swtchart.ISeriesLabel#getFont()
-	 */
-	public Font getFont() {
-		return font;
-	}
+    /*
+     * @see ISeriesLabel#setFont(Font)
+     */
+    public void setFont(Font font) {
+        if (font != null && font.isDisposed()) {
+            SWT.error(SWT.ERROR_INVALID_ARGUMENT);
+        }
+        if (font == null) {
+            this.font = Display.getDefault().getSystemFont();
+        } else {
+            this.font = font;
+        }
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.swtchart.ISeriesLabel#setFont(org.eclipse.swt.graphics.Font)
-	 */
-	public void setFont(Font font) {
-		if (font != null && font.isDisposed()) {
-			SWT.error(SWT.ERROR_INVALID_ARGUMENT);
-		}
-		if (font == null) {
-			this.font = Display.getDefault().getSystemFont();
-		} else {
-			this.font = font;
-		}
-	}
+    /*
+     * @see ISeriesLabel#isVisible()
+     */
+    public boolean isVisible() {
+        return isVisible;
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.swtchart.ISeriesLabel#isVisible()
-	 */
-	public boolean isVisible() {
-		return isVisible;
-	}
+    /*
+     * @see ISeriesLabel#setVisible(boolean)
+     */
+    public void setVisible(boolean visible) {
+        this.isVisible = visible;
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.swtchart.ISeriesLabel#setVisible(boolean)
-	 */
-	public void setVisible(boolean visible) {
-		this.isVisible = visible;
-	}
+    /**
+     * Draws series label.
+     * 
+     * @param gc
+     *            the GC object
+     * @param h
+     *            the horizontal coordinate to draw label
+     * @param v
+     *            the vertical coordinate to draw label
+     * @param ySeriesValue
+     *            the Y series value
+     */
+    protected void draw(GC gc, int h, int v, double ySeriesValue) {
+        if (!isVisible) {
+            return;
+        }
 
-	/**
-	 * Draws series label.
-	 * 
-	 * @param gc
-	 *            the GC object
-	 * @param h
-	 *            the horizontal coordinate to draw label
-	 * @param v
-	 *            the vertical coordinate to draw label
-	 * @param ySeriesValue
-	 *            the Y series value
-	 */
-	protected void draw(GC gc, int h, int v, double ySeriesValue) {
-		if (!isVisible) {
-			return;
-		}
-
-		gc.setForeground(color);
-		gc.setFont(font);
-		String text = new DecimalFormat(format).format(ySeriesValue);
-		gc.drawString(text, h, v, true);
-	}
+        gc.setForeground(color);
+        gc.setFont(font);
+        String text = new DecimalFormat(format).format(ySeriesValue);
+        gc.drawString(text, h, v, true);
+    }
 }
