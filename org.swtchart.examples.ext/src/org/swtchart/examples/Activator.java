3d5db5b20a890aea60016be60710e3b46f25a9fd
yoshitaka <yoshitaka@d30752db-1043-0410-9340-8085d4ee3fb9>
2009-03-01 21:51:52 +0000

    git-svn-id: https://swt-chart.svn.sourceforge.net/svnroot/swt-chart/trunk@147 d30752db-1043-0410-9340-8085d4ee3fb9

--- org.swtchart.examples.ext/src/org/swtchart/examples/Activator.java
+++ org.swtchart.examples.ext/src/org/swtchart/examples/Activator.java
@@ -8,51 +8,43 @@ import org.osgi.framework.BundleContext;
  */
 public class Activator extends AbstractUIPlugin {
 
-	/** The shared instance */
-	private static Activator plugin;
+    /** The shared instance */
+    private static Activator plugin;
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext
-	 * )
-	 */
-	@Override
-	public void start(BundleContext context) throws Exception {
-		super.start(context);
-		setPlugin(this);
-	}
+    /*
+     * @see AbstractUIPlugin#start(BundleContext)
+     */
+    @Override
+    public void start(BundleContext context) throws Exception {
+        super.start(context);
+        setPlugin(this);
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext
-	 * )
-	 */
-	@Override
-	public void stop(BundleContext context) throws Exception {
-		setPlugin(null);
-		super.stop(context);
-	}
+    /*
+     * @see AbstractUIPlugin#stop(BundleContext)
+     */
+    @Override
+    public void stop(BundleContext context) throws Exception {
+        setPlugin(null);
+        super.stop(context);
+    }
 
-	/**
-	 * Sets the shared instance
-	 * 
-	 * @param activator
-	 *            the shared instance
-	 */
-	private static void setPlugin(Activator activator) {
-		plugin = activator;
-	}
+    /**
+     * Sets the shared instance
+     * 
+     * @param activator
+     *            the shared instance
+     */
+    private static void setPlugin(Activator activator) {
+        plugin = activator;
+    }
 
-	/**
-	 * Returns the shared instance
-	 * 
-	 * @return the shared instance
-	 */
-	public static Activator getDefault() {
-		return plugin;
-	}
+    /**
+     * Returns the shared instance
+     * 
+     * @return the shared instance
+     */
+    public static Activator getDefault() {
+        return plugin;
+    }
 }
--- org.swtchart.examples.ext/src/org/swtchart/examples/ext/InteractiveChartExample.java
+++ org.swtchart.examples.ext/src/org/swtchart/examples/ext/InteractiveChartExample.java
@@ -17,67 +17,61 @@ import org.swtchart.ext.InteractiveChart;
  */
 public class InteractiveChartExample extends ViewPart {
 
-	private static final String[] categorySeries = { "Mon", "Tue", "Wed",
-			"Thu", "Fri" };
-	private static final double[] yLineSeries1 = { 4.6, 5.4, 6.9, 5.6, 7.1 };
-	private static final double[] yLineSeries2 = { 6.0, 5.1, 4.9, 5.3, 4.2 };
-	private static final double[] yBarSeries1 = { 1.1, 2.9, 3.3, 4.4, 3.5 };
-	private static final double[] yBarSeries2 = { 4.3, 3.4, 2.8, 2.1, 1.9 };
+    private static final String[] categorySeries = { "Mon", "Tue", "Wed",
+            "Thu", "Fri" };
+    private static final double[] yLineSeries1 = { 4.6, 5.4, 6.9, 5.6, 7.1 };
+    private static final double[] yLineSeries2 = { 6.0, 5.1, 4.9, 5.3, 4.2 };
+    private static final double[] yBarSeries1 = { 1.1, 2.9, 3.3, 4.4, 3.5 };
+    private static final double[] yBarSeries2 = { 4.3, 3.4, 2.8, 2.1, 1.9 };
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * org.eclipse.ui.part.WorkbenchPart#createPartControl(org.eclipse.swt.widgets
-	 * .Composite)
-	 */
-	@Override
-	public void createPartControl(Composite parent) {
-		parent.setLayout(new FillLayout());
+    /*
+     * @see WorkbenchPart#createPartControl(Composite)
+     */
+    @Override
+    public void createPartControl(Composite parent) {
+        parent.setLayout(new FillLayout());
 
-		// create an interactive chart
-		Chart chart = new InteractiveChart(parent, SWT.NONE);
+        // create an interactive chart
+        Chart chart = new InteractiveChart(parent, SWT.NONE);
 
-		// set title
-		chart.getTitle().setText("Sample Interactive Chart");
+        // set title
+        chart.getTitle().setText("Sample Interactive Chart");
 
-		// set category series
-		chart.getAxisSet().getXAxis(0).enableCategory(true);
-		chart.getAxisSet().getXAxis(0).setCategorySeries(categorySeries);
+        // set category series
+        chart.getAxisSet().getXAxis(0).enableCategory(true);
+        chart.getAxisSet().getXAxis(0).setCategorySeries(categorySeries);
 
-		// create line series 1
-		ILineSeries lineSeries1 = (ILineSeries) chart.getSeriesSet()
-				.createSeries(SeriesType.LINE, "line series 1");
-		lineSeries1.setYSeries(yLineSeries1);
+        // create line series 1
+        ILineSeries lineSeries1 = (ILineSeries) chart.getSeriesSet()
+                .createSeries(SeriesType.LINE, "line series 1");
+        lineSeries1.setYSeries(yLineSeries1);
 
-		// create line series 2
-		ILineSeries lineSeries2 = (ILineSeries) chart.getSeriesSet()
-				.createSeries(SeriesType.LINE, "line series 2");
-		lineSeries2.setYSeries(yLineSeries2);
-		lineSeries2.setLineColor(new Color(Display.getDefault(), 255, 0, 0));
+        // create line series 2
+        ILineSeries lineSeries2 = (ILineSeries) chart.getSeriesSet()
+                .createSeries(SeriesType.LINE, "line series 2");
+        lineSeries2.setYSeries(yLineSeries2);
+        lineSeries2.setLineColor(new Color(Display.getDefault(), 255, 0, 0));
 
-		// create bar series 1
-		IBarSeries barSeries1 = (IBarSeries) chart.getSeriesSet().createSeries(
-				SeriesType.BAR, "bar series 1");
-		barSeries1.setYSeries(yBarSeries1);
+        // create bar series 1
+        IBarSeries barSeries1 = (IBarSeries) chart.getSeriesSet().createSeries(
+                SeriesType.BAR, "bar series 1");
+        barSeries1.setYSeries(yBarSeries1);
 
-		// create bar series 2
-		IBarSeries barSeries2 = (IBarSeries) chart.getSeriesSet().createSeries(
-				SeriesType.BAR, "bar series 2");
-		barSeries2.setYSeries(yBarSeries2);
-		barSeries2.setBarColor(new Color(Display.getDefault(), 80, 240, 180));
+        // create bar series 2
+        IBarSeries barSeries2 = (IBarSeries) chart.getSeriesSet().createSeries(
+                SeriesType.BAR, "bar series 2");
+        barSeries2.setYSeries(yBarSeries2);
+        barSeries2.setBarColor(new Color(Display.getDefault(), 80, 240, 180));
 
-		// adjust the axis range
-		chart.getAxisSet().adjustRange();
-	}
+        // adjust the axis range
+        chart.getAxisSet().adjustRange();
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.eclipse.ui.part.WorkbenchPart#setFocus()
-	 */
-	@Override
-	public void setFocus() {
-		// do nothing
-	}
+    /*
+     * @see WorkbenchPart#setFocus()
+     */
+    @Override
+    public void setFocus() {
+        // do nothing
+    }
 }
