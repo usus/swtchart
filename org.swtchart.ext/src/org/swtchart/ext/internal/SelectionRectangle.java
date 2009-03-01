80abc3cb7de667ed6a71a4a518a7862aaac77b01
yoshitaka <yoshitaka@d30752db-1043-0410-9340-8085d4ee3fb9>
2009-03-01 21:51:17 +0000

    git-svn-id: https://swt-chart.svn.sourceforge.net/svnroot/swt-chart/trunk@146 d30752db-1043-0410-9340-8085d4ee3fb9

--- org.swtchart.ext/src/org/swtchart/ext/internal/SelectionRectangle.java
+++ org.swtchart.ext/src/org/swtchart/ext/internal/SelectionRectangle.java
@@ -2,129 +2,115 @@ package org.swtchart.ext.internal;
 
 import org.eclipse.swt.graphics.GC;
 import org.eclipse.swt.graphics.Point;
-import org.eclipse.swt.graphics.Rectangle;
-import org.swtchart.Range;
-import org.swtchart.IAxis.Direction;
 
 /**
  * Selection rectangle with mouse to zoom in/out.
  */
 public class SelectionRectangle {
 
-	private Point startPoint;
+    private Point startPoint;
 
-	private Point endPoint;
+    private Point endPoint;
 
-	/**
-	 * Sets the start point.
-	 * 
-	 * @param x
-	 *            the X coordinate of start point in pixels
-	 * @param y
-	 *            the Y coordinate of start point in pixels
-	 */
-	public void setStartPoint(int x, int y) {
-		startPoint = new Point(x, y);
-	}
+    /**
+     * Sets the start point.
+     * 
+     * @param x
+     *            the X coordinate of start point in pixels
+     * @param y
+     *            the Y coordinate of start point in pixels
+     */
+    public void setStartPoint(int x, int y) {
+        startPoint = new Point(x, y);
+    }
 
-	/**
-	 * Sets the end point.
-	 * 
-	 * @param x
-	 *            the X coordinate of end point in pixels
-	 * @param y
-	 *            the X coordinate of end point in pixels
-	 */
-	public void setEndPoint(int x, int y) {
-		endPoint = new Point(x, y);
-	}
+    /**
+     * Sets the end point.
+     * 
+     * @param x
+     *            the X coordinate of end point in pixels
+     * @param y
+     *            the X coordinate of end point in pixels
+     */
+    public void setEndPoint(int x, int y) {
+        endPoint = new Point(x, y);
+    }
 
-	/**
-	 * Gets the range with ratio.
-	 * 
-	 * @param direction
-	 *            the axis direction
-	 * @param rectangle
-	 *            the rectangle for plot area
-	 * @return the X range with ratio
-	 */
-	public Range getRange(Direction direction, Rectangle rectangle) {
-		if (endPoint == null) {
-			return null;
-		}
+    /**
+     * Gets the horizontal selected range.
+     * 
+     * @return the horizontal selected range in pixels
+     */
+    public Point getHorizontalRange() {
+        if (startPoint == null || endPoint == null) {
+            return null;
+        }
 
-		double start = 0;
-		double end = 0;
-		if (direction == Direction.X) {
-			start = (startPoint.x - rectangle.x) / (double) rectangle.width;
-			end = (endPoint.x - rectangle.x) / (double) rectangle.width;
-		} else if (direction == Direction.Y) {
-			start = (startPoint.y - rectangle.y) / (double) rectangle.height;
-			end = (endPoint.y - rectangle.y) / (double) rectangle.height;
-		}
+        return new Point(startPoint.x, endPoint.x);
+    }
 
-		double min;
-		double max;
-		if (start < end) {
-			min = start;
-			max = end;
-		} else {
-			min = end;
-			max = start;
-		}
+    /**
+     * Gets the vertical selected range.
+     * 
+     * @return the vertical selected range in pixels
+     */
+    public Point getVerticalRange() {
+        if (startPoint == null || endPoint == null) {
+            return null;
+        }
 
-		return new Range(min, max);
-	}
+        return new Point(startPoint.y, endPoint.y);
+    }
 
-	/**
-	 * Check if selection is disposed.
-	 * 
-	 * @return true if selection is disposed.
-	 */
-	public boolean isDisposed() {
-		return startPoint == null;
-	}
+    /**
+     * Check if selection is disposed.
+     * 
+     * @return true if selection is disposed.
+     */
+    public boolean isDisposed() {
+        return startPoint == null;
+    }
 
-	/**
-	 * Disposes the resource.
-	 */
-	public void dispose() {
-		startPoint = null;
-		endPoint = null;
-	}
+    /**
+     * Disposes the resource.
+     */
+    public void dispose() {
+        startPoint = null;
+        endPoint = null;
+    }
 
-	/**
-	 * Draws the selection rectangle on chart panel.
-	 * 
-	 * @param gc
-	 *            the graphics context
-	 */
-	public void draw(GC gc) {
-		if (startPoint == null || endPoint == null) {
-			return;
-		}
+    /**
+     * Draws the selection rectangle on chart panel.
+     * 
+     * @param gc
+     *            the graphics context
+     */
+    public void draw(GC gc) {
+        if (startPoint == null || endPoint == null) {
+            return;
+        }
 
-		int minX;
-		int maxX;
-		if (startPoint.x > endPoint.x) {
-			minX = endPoint.x;
-			maxX = startPoint.x;
-		} else {
-			minX = startPoint.x;
-			maxX = endPoint.x;
-		}
+        int minX;
+        int maxX;
+        if (startPoint.x > endPoint.x) {
+            minX = endPoint.x;
+            maxX = startPoint.x;
+        } else {
+            minX = startPoint.x;
+            maxX = endPoint.x;
+        }
 
-		int minY;
-		int maxY;
-		if (startPoint.y > endPoint.y) {
-			minY = endPoint.y;
-			maxY = startPoint.y;
-		} else {
-			minY = startPoint.y;
-			maxY = endPoint.y;
-		}
+        int minY;
+        int maxY;
+        if (startPoint.y > endPoint.y) {
+            minY = endPoint.y;
+            maxY = startPoint.y;
+        } else {
+            minY = startPoint.y;
+            maxY = endPoint.y;
+        }
 
-		gc.drawRectangle(minX, minY, maxX - minX, maxY - minY);
-	}
+        gc.drawRectangle(minX, minY, maxX - minX, maxY - minY);
+    }
 
 }
